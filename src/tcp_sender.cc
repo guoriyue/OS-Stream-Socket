dd8da7365e5027ce19d1f9c77d3dc984a3e121cf
guoriyue <1800012773@pku.edu.cn>
2023-04-29 00:32:41 +0000

    frame

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -12,14 +12,12 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  return sequence_numbers_in_flight_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return consecutive_retransmissions_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
@@ -30,24 +28,63 @@ optional<TCPSenderMessage> TCPSender::maybe_send()
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  // generates as many TCPSenderMessages as possible, as long as there 
+  // are new bytes to be read and space available in the window
+
+  // // Your code here.
+  // (void)outbound_stream;
+
+  TCPSenderMessage sender_message;
+  sender_message.payload = Buffer(outbound_stream.peek());
+
+  // outbound_stream.pop()
+  // std::string_view outbound_stream.peek()
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-  // Your code here.
-  return {};
+  // generate and send a zero-length message with the sequence number set correctly
+  TCPSenderMessage sender_message;
+  sender_message.seqno = Wrap32(0).wrap(next_abs_seqno_, isn_);
+  send_messages_.push_back(sender_message);
+  return sender_message;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  uint64_t abs_ackno = Wrap32(msg.ackno).unwrap(isn_, next_abs_seqno_);
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  time_elapsed_ += ms_since_last_tick;
+  if (time_elapsed_ >= retransmission_timeout_) {
+    send_messages_.push_back(outstanding_messages_[0]);
+    if (receiver_window_size_) {
+      consecutive_retransmissions_+=1;
+      retransmission_timeout_ *= 2;
+    }
+    time_elapsed_ = 0;
+  }
 }
+
+// struct TCPSenderMessage
+// {
+//   Wrap32 seqno { 0 };
+//   bool SYN { false };
+//   Buffer payload {};
+//   bool FIN { false };
+
+//   // How many sequence numbers does this segment use?
+//   size_t sequence_length() const { return SYN + payload.size() + FIN; }
+// };
+
+
+
+
+
+// struct TCPReceiverMessage
+// {
+//   std::optional<Wrap32> ackno {};
+//   uint16_t window_size {};
+// };
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -7,8 +7,20 @@
 class TCPSender
 {
   Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  uint64_t initial_RTO_ms_; // initial retransmission timeout
 
+  std::vector<TCPSenderMessage> send_messages_ = {};
+  std::vector<TCPSenderMessage> outstanding_messages_ = {};
+
+  uint64_t next_abs_seqno_ = 0;
+
+  uint64_t sequence_numbers_in_flight_ = 0;
+  uint64_t consecutive_retransmissions_ = 0;
+  uint64_t retransmission_timeout_= 0;
+
+  size_t time_elapsed_ = 0;
+
+    
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
